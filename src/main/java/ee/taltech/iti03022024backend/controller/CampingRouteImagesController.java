f3d2ae81e8aef9bb786db479fa94209e72f13804
Rihard Rivis <rihard.rivis@proton.me>
2024-10-28 19:59:57 +0200

    feat: image names endpoint

--- src/main/java/ee/taltech/iti03022024backend/controller/CampingRouteImagesController.java
+++ src/main/java/ee/taltech/iti03022024backend/controller/CampingRouteImagesController.java
@@ -1,13 +1,11 @@
 package ee.taltech.iti03022024backend.controller;
 
+import ee.taltech.iti03022024backend.dto.CampingRouteImageNamesDto;
 import ee.taltech.iti03022024backend.service.CampingRouteImagesService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 
@@ -18,11 +16,16 @@ import org.springframework.web.multipart.MultipartFile;
 public class CampingRouteImagesController {
     private final CampingRouteImagesService campingRouteImagesService;
 
-    @PostMapping()
+    @PostMapping("/{id}")
     public ResponseEntity<Void> addImagesToCampingRoute(
             @RequestParam("files") MultipartFile[] files,
-            @RequestParam long id
+            @PathVariable long id
     ) {
         return campingRouteImagesService.storeImages(files, id);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CampingRouteImageNamesDto> getImageNames(@PathVariable long id) {
+        return campingRouteImagesService.getImageNames(id);
     }
 }
--- src/main/java/ee/taltech/iti03022024backend/service/CampingRouteImagesService.java
+++ src/main/java/ee/taltech/iti03022024backend/service/CampingRouteImagesService.java
@@ -1,5 +1,6 @@
 package ee.taltech.iti03022024backend.service;
 
+import ee.taltech.iti03022024backend.dto.CampingRouteImageNamesDto;
 import ee.taltech.iti03022024backend.exception.CampingRouteImageStorageException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -18,20 +20,50 @@ import java.nio.file.Path;
 public class CampingRouteImagesService {
     private final Path rootDir = Path.of("files").resolve("camping_route_images");
 
-    public ResponseEntity<Void> storeImages(MultipartFile[] files, long postId) {
+    public ResponseEntity<Void> storeImages(MultipartFile[] files, long campingRouteId) {
         log.info("Storing images for camping route with id {}. image count: {}",
-                postId,
+                campingRouteId,
                 files.length
         );
 
         for (MultipartFile file : files) {
-            storeImage(file, postId);
+            storeImage(file, campingRouteId);
         }
 
         return ResponseEntity.ok().build();
     }
 
-    private void storeImage(MultipartFile file, long postId) {
+    public ResponseEntity<CampingRouteImageNamesDto> getImageNames(long campingRouteId) {
+        log.info("Fetching image names for camping route with id {}", campingRouteId);
+
+        var dirName = rootDir.resolve(String.valueOf(campingRouteId))
+                .normalize()
+                .toAbsolutePath();
+
+        if (Files.notExists(dirName)) {
+            log.info("No images where found for camping route with id {}", campingRouteId);
+            return ResponseEntity.notFound().build();
+        }
+
+        // find all files in the dir. get their names and construct dto with list of these names
+        try (var stream = Files.list(dirName)) {
+            var fileNames = stream
+                    .filter(file -> !Files.isDirectory(file))
+                    .map(Path::getFileName)
+                    .map(Path::toString)
+                    .toList();
+            var response = new CampingRouteImageNamesDto();
+            response.setImageNames(fileNames);
+            return ResponseEntity.ok(response);
+        } catch (IOException e) {
+            throw new CampingRouteImageStorageException(
+                    "Could not find files for camping route with id " + campingRouteId,
+                    e
+            );
+        }
+    }
+
+    private void storeImage(MultipartFile file, long campingRouteId) {
         log.info("Storing file with name {}", file.getOriginalFilename());
 
         if (file.isEmpty()) {
@@ -42,24 +74,21 @@ public class CampingRouteImagesService {
             var fileExtension = FilenameUtils.getExtension(file.getOriginalFilename());
 
             // create directories if they don't exist
-            var campingRouteDir = rootDir.resolve(String.valueOf(postId));
+            var campingRouteDir = rootDir.resolve(String.valueOf(campingRouteId));
             if (Files.notExists(campingRouteDir)) {
-                log.info("Creating directory for camping route with id {}", postId);
+                log.info("Creating directory for camping route with id {}", campingRouteId);
                 Files.createDirectories(campingRouteDir);
             }
 
             // save the file to the directory
-            try (var stream = Files.list(campingRouteDir)) {
-                var currentFileCount = stream.count();
+            Files.copy(
+                    file.getInputStream(),
+                    campingRouteDir
+                            .resolve(UUID.randomUUID() + "." + fileExtension)
+                            .normalize()
+                            .toAbsolutePath() // EX: files/{id}/as43sl5bf24.png
+            );
 
-                Files.copy(
-                        file.getInputStream(),
-                        campingRouteDir
-                                .resolve(currentFileCount + "." + fileExtension)
-                                .normalize()
-                                .toAbsolutePath() // EX: files/{id}/0.png
-                );
-            }
         } catch (IOException e) {
             throw new CampingRouteImageStorageException("File could not be saved.", e);
         }
