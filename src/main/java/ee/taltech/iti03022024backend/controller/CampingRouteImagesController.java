aca9828834da21388d05eefe3e793570943f10fd
Rihard Rivis <rihard.rivis@proton.me>
2024-10-28 20:15:13 +0200

    feat: can get file by name

--- src/main/java/ee/taltech/iti03022024backend/controller/CampingRouteImagesController.java
+++ src/main/java/ee/taltech/iti03022024backend/controller/CampingRouteImagesController.java
@@ -4,6 +4,7 @@ import ee.taltech.iti03022024backend.dto.CampingRouteImageNamesDto;
 import ee.taltech.iti03022024backend.service.CampingRouteImagesService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -27,5 +28,10 @@ public class CampingRouteImagesController {
     @GetMapping("/{id}")
     public ResponseEntity<CampingRouteImageNamesDto> getImageNames(@PathVariable long id) {
         return campingRouteImagesService.getImageNames(id);
+    }
+
+    @GetMapping("/{id}/{imageName}")
+    public ResponseEntity<Resource> getImage(@PathVariable long id, @PathVariable String imageName) {
+        return campingRouteImagesService.getImage(id, imageName);
     }
 }
--- src/main/java/ee/taltech/iti03022024backend/service/CampingRouteImagesService.java
+++ src/main/java/ee/taltech/iti03022024backend/service/CampingRouteImagesService.java
@@ -5,11 +5,15 @@ import ee.taltech.iti03022024backend.exception.CampingRouteImageStorageException
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FilenameUtils;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.UUID;
@@ -91,6 +95,40 @@ public class CampingRouteImagesService {
 
         } catch (IOException e) {
             throw new CampingRouteImageStorageException("File could not be saved.", e);
+        }
+    }
+
+    public ResponseEntity<Resource> getImage(long id, String imageName) {
+        log.info("Getting file with name {} for camping route with id {}", imageName, id);
+
+        // file path in the system
+        var fileDir = rootDir
+                .resolve(String.valueOf(id))
+                .resolve(imageName)
+                .normalize()
+                .toAbsolutePath();
+
+        try {
+            // get the file as a "Resource" and send it back to client.
+            var resource = new UrlResource(fileDir.toUri());
+            if (!resource.exists() || !resource.isReadable()) {
+                throw new CampingRouteImageStorageException("Could not read file: " + resource.getFilename());
+            }
+            return ResponseEntity
+                    .ok()
+                    .header(
+                            HttpHeaders.CONTENT_DISPOSITION,
+                            "attachment; filename=\"" + resource.getFilename() + "\"")
+                    .body(resource);
+
+        } catch (MalformedURLException e) {
+            throw new CampingRouteImageStorageException(
+                    "File with name " +
+                            imageName +
+                            " does not exist for post with id "
+                            + id,
+                    e
+            );
         }
     }
 }
