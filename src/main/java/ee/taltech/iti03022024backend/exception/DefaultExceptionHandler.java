b38e3b21848e5e5ba0d353f1868fb5237df93e81
Rasmus <rraasu@taltech.ee>
2024-10-30 14:54:47 +0200

    fix: Standardized the exception handling.

--- src/main/java/ee/taltech/iti03022024backend/exception/DefaultExceptionHandler.java
+++ src/main/java/ee/taltech/iti03022024backend/exception/DefaultExceptionHandler.java
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @Slf4j
 public class DefaultExceptionHandler {
 
-    @ExceptionHandler({CampingRouteNotFoundException.class, UserNotFoundException.class})
+    @ExceptionHandler({
+            CampingRouteNotFoundException.class,
+            UserNotFoundException.class,
+            CampingRouteImageNotFound.class})
     public ResponseEntity<ExceptionResponse> handleNotFoundException(RuntimeException e) {
         log.warn("Resource not found: {}", e.getMessage());
         return ResponseEntity
@@ -18,7 +21,9 @@ public class DefaultExceptionHandler {
                 .body(new ExceptionResponse(e.getMessage()));
     }
 
-    @ExceptionHandler({EmailAlreadyExistsException.class, UsernameAlreadyExistsException.class})
+    @ExceptionHandler({
+            EmailAlreadyExistsException.class,
+            UsernameAlreadyExistsException.class})
     public ResponseEntity<ExceptionResponse> handleAlreadyExistsException(RuntimeException e) {
         log.warn("Resouor all new exceptions.rce already exists: {}", e.getMessage());
         return ResponseEntity
@@ -26,7 +31,8 @@ public class DefaultExceptionHandler {
                 .body(new ExceptionResponse(e.getMessage()));
     }
 
-    @ExceptionHandler({InvalidPasswordException.class, InvalidCredentialsException.class})
+    @ExceptionHandler({InvalidPasswordException.class,
+            InvalidCredentialsException.class})
     public ResponseEntity<ExceptionResponse> handleInvalidInformationExcetion(RuntimeException e) {
         log.warn("Given information is invalid: {}", e.getMessage());
         return ResponseEntity
@@ -35,21 +41,15 @@ public class DefaultExceptionHandler {
     }
 
     @ExceptionHandler(CampingRouteImageStorageException.class)
-    public ResponseEntity<ExceptionResponse> exception(CampingRouteImageStorageException e) {
-        log.error("Camping route image storage exception occurred", e);
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ExceptionResponse(e.getMessage()));
+    public ResponseEntity<ExceptionResponse> handleStorageException(CampingRouteImageStorageException e) {
+        log.error("Storage exception occurred: {}", e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ExceptionResponse(e.getMessage()));
     }
-
-    @ExceptionHandler(CampingRouteImageNotFound.class)
-    public ResponseEntity<ExceptionResponse> exception(CampingRouteImageNotFound e) {
-        log.error("Camping route image not found", e);
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ExceptionResponse(e.getMessage()));
-    }
-
 
     @ExceptionHandler(RuntimeException.class)
     public void exception(RuntimeException e) {
         log.error("An exception occurred.", e);
     }
-
 }
