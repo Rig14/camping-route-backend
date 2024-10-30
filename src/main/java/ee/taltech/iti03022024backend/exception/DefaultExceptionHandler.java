7f710d6b6ab598992f542e8b257ef1cf82c64c07
Rasmus <rraasu@taltech.ee>
2024-10-30 14:29:25 +0200

    feat: Added exception handling for all new exceptions.

--- src/main/java/ee/taltech/iti03022024backend/exception/DefaultExceptionHandler.java
+++ src/main/java/ee/taltech/iti03022024backend/exception/DefaultExceptionHandler.java
@@ -9,9 +9,28 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 @Slf4j
 public class DefaultExceptionHandler {
-    @ExceptionHandler(CampingRouteNotFoundException.class)
-    public ResponseEntity<ExceptionResponse> exception(CampingRouteNotFoundException e) {
-        log.warn("Camping route error occurred", e);
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ExceptionResponse(e.getMessage()));
+
+    @ExceptionHandler({CampingRouteNotFoundException.class, UserNotFoundException.class})
+    public ResponseEntity<ExceptionResponse> handleNotFoundException(RuntimeException e) {
+        log.warn("Resource not found: {}", e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ExceptionResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler({EmailAlreadyExistsException.class, UsernameAlreadyExistsException.class})
+    public ResponseEntity<ExceptionResponse> handleAlreadyExistsException(RuntimeException e) {
+        log.warn("Resource already exists: {}", e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(new ExceptionResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler({InvalidPasswordException.class, InvalidCredentialsException.class})
+    public ResponseEntity<ExceptionResponse> handleInvalidInformationExcetion(RuntimeException e) {
+        log.warn("Given information is invalid: {}", e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ExceptionResponse(e.getMessage()));
     }
 }
