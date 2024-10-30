003708945d2f3eed1e3650cb3d25875b4cc17d48
Rasmus <rraasu@taltech.ee>
2024-10-30 13:38:34 +0200

    feat: UserRepository.

---
+++ src/main/java/ee/taltech/iti03022024backend/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package ee.taltech.iti03022024backend.repository;
+
+import ee.taltech.iti03022024backend.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+    Optional<UserEntity> findByUsername(String username);
+}
