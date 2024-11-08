6f886743ce5c304fcaf60fd901de72973715b616
Rasmus <rraasu@taltech.ee>
2024-11-08 19:21:50 +0200

    fix: fixed postgre table names.

--- src/main/java/ee/taltech/iti03022024backend/entity/CommentEntity.java
+++ src/main/java/ee/taltech/iti03022024backend/entity/CommentEntity.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 
 @Getter @Setter
 @Entity
-@Table(name = "Comment")
+@Table(name = "comment")
 public class CommentEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
