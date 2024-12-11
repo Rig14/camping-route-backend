00405ae03d8c0d3872bcdf6707a4fea6d17810e0
Artur R <reiljanartur@gmail.com>
2024-12-11 21:51:12 +0200

    feat: integraton tests setup

---
+++ src/test/java/ee/taltech/iti03022024backend/controller/CampingRouteControllerTest.java
@@ -0,0 +1,33 @@
+package ee.taltech.iti03022024backend.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CampingRouteControllerTest extends AbstractIntegrationTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void givenExistingCampingRoute_whenGetCampingRoute_thenReturnsOkAndCorrectData() throws Exception {
+        mvc.perform(get("/api/public/camping_routes/{id}", 1L)
+                        .with(user("user"))) // simulate authenticated user "user"
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void givenNonExistingCampingRoute_whenGetCampingRoute_thenReturnsNotFound() throws Exception {
+        mvc.perform(get("/api/public/camping_routes/{id}", 999L)
+                        .with(user("user")))
+                .andExpect(status().isNotFound());
+    }
+}
